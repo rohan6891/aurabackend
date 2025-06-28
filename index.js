e59cfb4c5f23149f97acaa38ae3f400cd7848c27
rohan6891 <rohan5144320@gmail.com>
2025-06-29 05:26:18 +0530

    added cors

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({ 'origin': '*' }));
 app.use(express.json());
 
 // MongoDB Connection
